57bc31fc7b7c7c5eaa8cd1f6210dad0a18153b7d
genius.choi <csj4032@gmail.com>
2019-01-10 21:52:49 +0900

    Image Create

--- backend/src/main/java/com/genius/backend/interfaces/GeniusController.java
+++ backend/src/main/java/com/genius/backend/interfaces/GeniusController.java
@@ -1,26 +1,25 @@
 package com.genius.backend.interfaces;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import net.imglib2.FinalInterval;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgFactory;
+import net.imglib2.type.numeric.integer.UnsignedShortType;
+import net.imglib2.util.Intervals;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.activation.FileTypeMap;
-import javax.servlet.ServletContext;
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.nio.file.Files;
 
 @Slf4j
 @Controller
 public class GeniusController {
-
-	@Autowired
-	private ServletContext servletContext;
 
 	@GetMapping("/greeting")
 	public String greeting() {
@@ -28,12 +27,16 @@ public class GeniusController {
 	}
 
 	@GetMapping("/picture/{width}/{height}/{extension:jpg|png|gif}")
-	public ResponseEntity<byte[]> picture(HttpServletResponse response, @PathVariable(name = "width") Integer width, @PathVariable(name = "width") Integer height, @PathVariable(name = "extension", required = false) String extension) throws IOException {
-		File images = new File("/resources/static/images/20181231_121003_029.jpg");
-		response.setContentType(MediaType.IMAGE_JPEG_VALUE);
-		return ResponseEntity.ok()
-				.header("Content-Disposition", "attachment; filename=" + images.getName())
-				.contentType(MediaType.valueOf(FileTypeMap.getDefaultFileTypeMap().getContentType(images)))
-				.body(Files.readAllBytes(images.toPath()));
+	public void picture(HttpServletResponse response,
+						@PathVariable(name = "width") Integer width,
+						@PathVariable(name = "height") Integer height,
+						@PathVariable(name = "extension", required = false) String extension) throws IOException {
+		BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D graphics = (Graphics2D) bufferedImage.getGraphics();
+		graphics.setColor(Color.GRAY);
+		graphics.fillRect(0, 0, width, height);
+		response.setContentType(MediaType.IMAGE_PNG_VALUE);
+		ImageIO.write(bufferedImage, extension, response.getOutputStream());
+		response.flushBuffer();
 	}
 }
