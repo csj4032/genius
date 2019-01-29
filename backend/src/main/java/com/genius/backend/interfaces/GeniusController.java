0e0d3cb2c0775f4d5de75cfb0fb009feb8ce5ce4
genius.choi <genius.choi@wemakeprice.com>
2019-01-29 16:57:50 +0900

    facebook callback

--- backend/src/main/java/com/genius/backend/interfaces/GeniusController.java
+++ backend/src/main/java/com/genius/backend/interfaces/GeniusController.java
@@ -1,6 +1,7 @@
 package com.genius.backend.interfaces;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -10,10 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GeniusController {
 
+	@Value("${spring.application.url}")
+	private String applicationUrl;
+
 	@PreAuthorize("hasRole('ROLE_USER')")
 	@GetMapping(value = {"/", "/greeting"})
 	public String greeting(ModelMap modelMap) {
-		modelMap.addAttribute("model", "123");
+		modelMap.addAttribute("applicationUrl", applicationUrl);
 		return "greeting";
 	}
 }
---
+++ backend/src/main/java/com/genius/backend/interfaces/api/facebook/CallbackController.java
@@ -0,0 +1,15 @@
+package com.genius.backend.interfaces.api.facebook;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1")
+@RestController
+public class CallbackController {
+
+	@GetMapping("/facebook/callback")
+	public String callBack(String message) {
+		return message;
+	}
+}
